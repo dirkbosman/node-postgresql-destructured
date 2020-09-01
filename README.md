5813efae52db27ab94e01080b6ca393147bb6918
Dirk Bosman <dirkjobosman@gmail.com>
2020-09-01 17:41:45 +0200

    update docs

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # node-postgresql-destructured
 
-
 #### Summary:
+
 - node express server, middleware and apis
 - postgresql relational database
 
 #### Boilerplates:
-- [Part 1: node-postgresql-unstructured](https://github.com/dirkbosman/node-postgresql-unstructured) 
+
+- [Part 1: node-postgresql-unstructured](https://github.com/dirkbosman/node-postgresql-unstructured)
 - [Part 2: node-postgresql-destructured](https://github.com/dirkbosman/node-postgresql-destructured)
 - [Part 3: node-postgresql-controllers](https://github.com/dirkbosman/node-postgresql-controllers)
+- [Part 4: node-mongodb-controllers](https://github.com/dirkbosman/node-mongodb-controllers)
 
 #### Node SQL sample project:
+
 1. Move the order routes to the `./api/orders.js` file. You will need the same imports in the `./api/users.js` routes. When you are done, you can delete the commented order routes in the `server.js`
 2. Create `post`, `delete` and `put` routes for orders like it's been done for users.
 3. Make sure the order routes are using the error handling middleware by calling the `next` function in every `catch` statement, like it's done for users.
--- api/orders.js
+++ api/orders.js
@@ -1,1 +1,52 @@
-// here go the orders routes
+// Create `post`, `delete` and `put` routes for orders (like it's been done for users).
+
+const express = require("express");
+const api = express.Router();
+
+const db = require("../dbinit");
+
+api.get("/", (req, res, next) => {
+  db.query("SELECT * FROM orders;")
+    .then((data) => res.json(data.rows))
+    .catch((e) => next(e));
+});
+
+api.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  db.query("SELECT * FROM orders WHERE id=$1;", [id])
+    .then((data) => res.json(data.rows))
+    .catch((e) => next(e));
+});
+
+// id  SERIAL PRIMARY KEY,
+// price float,
+// date timestamp,
+// user_id int,
+// FOREIGN KEY (user_id) REFERENCES users(id)
+
+api.post("/", (req, res, next) => {
+  const { name } = req.body;
+
+  db.query("INSERT INTO orders(first_name) values($1);", [name])
+    .then((data) => res.status(201).json(data))
+    .catch((e) => next(e));
+});
+
+api.delete("/:id", (req, res, next) => {
+  const { id } = req.params;
+
+  db.query("DELETE FROM orders WHERE id=$1;", [id])
+    .then((data) => res.status(201).json(data))
+    .catch((e) => next(e));
+});
+
+api.put("/:id", (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  db.query("UPDATE orders SET first_name=$1 WHERE id=$2;", [name, id])
+    .then((data) => res.status(201).json(data))
+    .catch((e) => next(e));
+});
+
+module.exports = api;
